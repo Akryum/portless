57715b0991f1e66183bb9310eb0d518e90573fd9
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-01-13 20:08:38 +0100

    fix(greenlock): staging

--- packages/cli/src/greenlock.ts
+++ packages/cli/src/greenlock.ts
@@ -1,10 +1,12 @@
 import Greenlock from 'greenlock'
 import consola from 'consola'
+import chalk from 'chalk'
 import fs from 'fs-extra'
 import path from 'path'
 import { PortlessConfig } from '@portless/config'
 import { getDomain } from './util/domain'
 import { getRcFolder } from './util/rc-folder'
+import { restartNgrokTunnels } from './ngrok'
 
 export interface GreenlockInfo {
   publicKeyId?: string
@@ -38,14 +40,19 @@ export async function setupGreenlock (config: PortlessConfig): Promise<Greenlock
     packageRoot,
     packageAgent: options.packageAgent,
     maintainerEmail: options.maintainerEmail,
-    server: options.staging,
+    staging: options.staging,
     notify: (event: string, details: any) => {
       if (event === 'error') {
         consola.error(details)
       } else if (event === 'warning') {
         consola.warn(details)
+      } else if (event === 'certificate_status') {
+        consola.info(chalk.blue(event), details)
+        if (details.status === 'valid') {
+          restartNgrokTunnels()
+        }
       } else {
-        consola.info(details)
+        consola.info(chalk.blue(event), details)
       }
     },
   })
@@ -57,11 +64,26 @@ export async function setupGreenlock (config: PortlessConfig): Promise<Greenlock
 
   await greenlock.add(site)
 
-  const accountFile = path.resolve(configDir, 'accounts/acme-v02.api.letsencrypt.org/directory', `${options.maintainerEmail}.json`)
-  const accountData = await fs.readJson(accountFile)
+  const accountFile = path.resolve(configDir, `accounts/acme${options.staging ? '-staging' : ''}-v02.api.letsencrypt.org/directory`, `${options.maintainerEmail}.json`)
+
+  async function readAccountData (): Promise<any> {
+    if (fs.existsSync(accountFile)) {
+      return fs.readJson(accountFile)
+    } else {
+      await wait(500)
+      return readAccountData()
+    }
+  }
+  const accountData = await readAccountData()
   const publicKeyId = accountData.publicKeyJwk.kid
 
   return {
     publicKeyId,
   }
+}
+
+function wait (ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
 }
