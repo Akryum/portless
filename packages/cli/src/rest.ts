8138bf1f002be910ea264c0700ee3d7120ea2a9e
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-01-15 13:36:15 +0100

    fix: cli commands using the API

--- packages/cli/src/rest.ts
+++ packages/cli/src/rest.ts
@@ -1,12 +1,23 @@
 import { loadGlobalConfig } from '@portless/global-config'
 import fetch, { RequestInit } from 'node-fetch'
+import consola from 'consola'
 
 export async function rest (method: string, url: string, options: RequestInit = {}) {
   const config = await loadGlobalConfig()
+  const body = options.body ? JSON.stringify(options.body) : undefined
   const result = await fetch(`http://${config.host || 'localhost'}:${config.port}${url}`, {
     method,
     ...options,
+    body,
+    headers: {
+      'content-type': 'application/json',
+    },
   })
+  if (!result.ok) {
+    const { error } = await result.json()
+    consola.error(error)
+    return
+  }
   return result.json()
 }
 
@@ -16,24 +27,24 @@ export async function get (url: string) {
 
 export async function post (url: string, data: any = null) {
   return rest('POST', url, {
-    body: JSON.stringify(data),
+    body: data,
   })
 }
 
 export async function put (url: string, data: any = null) {
   return rest('PUT', url, {
-    body: JSON.stringify(data),
+    body: data,
   })
 }
 
 export async function patch (url: string, data: any = null) {
   return rest('PATCH', url, {
-    body: JSON.stringify(data),
+    body: data,
   })
 }
 
 export async function del (url: string, data: any = null) {
   return rest('DELETE', url, {
-    body: JSON.stringify(data),
+    body: data,
   })
 }
